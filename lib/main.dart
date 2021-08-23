aa79950a8cbf18421532be65d31434aea5632636
lucasaobarbosa <lucasaobarbosa@gmai.com>
2021-08-23 20:34:36 -0300

    @override

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 
-main () {
-            //Criando uma instânica
-  runApp(new PerguntaApp());
+//Criando uma instânica
+main () => runApp(PerguntaApp());
 
-}
                           // Chamando a herança de Widget
 class PerguntaApp extends StatelessWidget {
 
-  //Criando método build
+  @override      //Criando método build
   Widget build(BuildContext context) {
     //Criando uma instância
     return MaterialApp(
-      home: Text('Olá Flutter!'),
+      home: Text('Olá Flutter!!'),
     );
   }
 }
