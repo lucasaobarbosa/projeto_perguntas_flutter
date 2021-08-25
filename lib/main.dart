454f701bb2f4ae0e7c6a71dfdce6e34d14a87c64
lucasaobarbosa <lucasaobarbosa@gmai.com>
2021-08-25 19:24:42 -0300

    Violando StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 
 //Criando uma instânica
 main () => runApp(PerguntaApp());
-
-void responder(){
-  print('Pergunta respondida!!!');
-}
-
                           // Chamando a herança de Widget
 class PerguntaApp extends StatelessWidget {
+
+  var perguntaSelecionada = 1;
+
+  void responder(){
+  perguntaSelecionada ++;
+  print(perguntaSelecionada);
+}
+
   final perguntas = [
     'Qual é a sua cor favorita?',
     'Qual é o seu animal favorito?',
@@ -24,20 +27,18 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text(perguntas[0]),
+            Text(perguntas[perguntaSelecionada]),
             ElevatedButton(
               child: Text('Resposta 1'),
               onPressed: responder,
             ),
             ElevatedButton(
               child: Text('Resposta 2'),
-              onPressed: () {
-                print('Resposta 2 foi selecionada!');
-              },
+              onPressed: responder,
             ),
             ElevatedButton(
               child: Text('Resposta 3'),
-              onPressed: () => print('Resposta 3!!!'),
+              onPressed: responder,
             ),
          ],
         ),
