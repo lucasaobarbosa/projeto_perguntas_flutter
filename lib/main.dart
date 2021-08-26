02ce9f33d81d1195e7163cda47f6acafb9557ed6
lucasaobarbosa <lucasaobarbosa@gmai.com>
2021-08-26 20:50:09 -0300

    componentes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 //Criando uma instânica
 main () => runApp(PerguntaApp());
@@ -27,8 +28,8 @@ class _PerguntaAppState extends State<PerguntaApp> {
           title: Text('Perguntas'),
         ),
         body: Column(
-          children: [
-            Text(perguntas[_perguntaSelecionada]),
+          children: <Widget>[
+            Questao(perguntas[_perguntaSelecionada]),
             ElevatedButton(
               child: Text('Resposta 1'),
               onPressed: _responder,
