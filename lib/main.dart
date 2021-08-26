c5988a47e52ddcbf20592548a604647be0a662cd
lucasaobarbosa <lucasaobarbosa@gmai.com>
2021-08-26 20:37:45 -0300

    Classe Privada

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 //Criando uma instânica
 main () => runApp(PerguntaApp());
                           
-class PerguntaAppState extends State<PerguntaApp> {
-  var perguntaSelecionada = 0;
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
 
-  void responder(){
+  void _responder(){
   setState(() {
-  perguntaSelecionada ++;   
+  _perguntaSelecionada ++;   
   });
-  print(perguntaSelecionada);
+  print(_perguntaSelecionada);
   }
 
   final perguntas = [
@@ -28,18 +28,18 @@ class PerguntaAppState extends State<PerguntaApp> {
         ),
         body: Column(
           children: [
-            Text(perguntas[perguntaSelecionada]),
+            Text(perguntas[_perguntaSelecionada]),
             ElevatedButton(
               child: Text('Resposta 1'),
-              onPressed: responder,
+              onPressed: _responder,
             ),
             ElevatedButton(
               child: Text('Resposta 2'),
-              onPressed: responder,
+              onPressed: _responder,
             ),
             ElevatedButton(
               child: Text('Resposta 3'),
-              onPressed: responder,
+              onPressed: _responder,
             ),
          ],
         ),
@@ -49,7 +49,7 @@ class PerguntaAppState extends State<PerguntaApp> {
 }                         
                           // Chamando a herança de Widget
 class PerguntaApp extends StatefulWidget {
-  PerguntaAppState createState(){
-    return PerguntaAppState();
+  _PerguntaAppState createState(){
+    return _PerguntaAppState();
   }
 }
