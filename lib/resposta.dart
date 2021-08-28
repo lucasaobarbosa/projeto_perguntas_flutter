0dfcc558d47b80b411ae89081075595740ff78fd
lucasaobarbosa <lucasaobarbosa@gmai.com>
2021-08-28 09:47:31 -0300

    style

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -2,14 +2,26 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget{
   final String texto;
+  final void Function() onSelection;
 
-  Resposta(this.texto);
+  Resposta(this.texto, this.onSelection);
 
  @override
  Widget build(BuildContext context) {
-   return ElevatedButton(
-    child: Text(texto),
-    onPressed: null,
-    );
+   return Container(
+     width: double.infinity,
+     child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+      primary: Colors.blue,
+      ),
+      child: Text(
+        texto,
+        style: TextStyle(
+        color: Colors.white,
+        ),
+      ),
+      onPressed: onSelection,
+     ),
+   );
   }
 }
