5dbd7b304a17d642d19aa3ce3a9cfc59dc665f8d
lucasaobarbosa <lucasaobarbosa@gmai.com>
2021-09-01 20:04:20 -0300

    projetoFinal

--- lib/resultado.dart
+++ lib/resultado.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
 
-class Resuldado extends StatelessWidget {
+class Resultado extends StatelessWidget {
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+
+  Resultado(this.pontuacao, this.quandoReiniciarQuestionario);
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return 'Parabéns!';
+    } else if (pontuacao < 12) {
+      return 'Você é bom';
+    } else if (pontuacao < 16) {
+      return 'Impressionante';
+    } else {
+      return 'Nível Jedi!';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
           child: Text(
-            'Parabéns!!',
+            fraseResultado,
             style: TextStyle(fontSize: 28),
-          ),   
+          ),
+        ),
+        TextButton(
+          child: Text(
+            'Reiniciar?',
+            style: TextStyle(fontSize: 18),
+          ),
+          onPressed: quandoReiniciarQuestionario,
+        ),
+      ],
     );
   }
 }
